eb7ee8c0d11d4d6baa2529a894599101980b3614
XiNG <1078059201@qq.com>
2019-10-13 22:35:48 +0800

    commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ stemmer  = LancasterStemmer()
 
 import numpy as np
 import tflearn
-import tensorflow
+import tensorflow as tf
 import random
 import json
 nltk.download('punkt')
@@ -23,13 +23,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -37,7 +37,7 @@ labels = sorted(labels)
 training = []
 output = []
 
-out_empty = [0 for _ in range(len(classes))]
+out_empty = [0 for _ in range(len(labels))]
 
 for x,doc in enumerate(docs_x):
     bag = []
@@ -58,3 +58,16 @@ for x,doc in enumerate(docs_x):
 
 training = np.array(training)
 output = np.array(output)
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
